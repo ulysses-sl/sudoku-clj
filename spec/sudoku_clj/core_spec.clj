424d629bcdb286b0443e2448436b4acc25187c3f
ulysses-sl <mail@saklee.net>
2015-01-18 14:40:38 -0500

    implemented board loading mechanism

--- spec/sudoku_clj/core_spec.clj
+++ spec/sudoku_clj/core_spec.clj
@@ -18,6 +18,10 @@
   (it "String \"1 2 3\" converts to row [1 2 3]"
     (println "Test 1. make-row-from-string")
     (should= '[1 2 3] (make-row-from-string "1 2 3")))
+
+  (it "String \"1 2 3  converts to board [[1 2 3]\n                                    4 5 6\"                    [4 5 6]]"
+    (println "Test 2. make-board-from-string")
+    (should= '[[1 2 3] [4 5 6]] (make-board-from-string "1 2 3\n4 5 6")))
 )
 
 (run-specs)
--- src/sudoku_clj/core.clj
+++ src/sudoku_clj/core.clj
@@ -1,5 +1,6 @@
 (ns sudoku-clj.core
-  (:gen-class))
+  (:gen-class)
+  (:require [clojure.string :as str]))
 
 (def board-file "board.txt")
 
@@ -10,12 +11,12 @@
 (defn make-row-from-string
   "Reconstructs a single row of sudoku board from the string"
   [row-string]
-  ":-(")
+  (->> (str/split row-string #" ") (map read-string) (apply vector)))
 
 (defn make-board-from-string
   "Reconstructs a sudoku board from the string"
   [board-string]
-  ":-(")
+  (->> board-string (str/split-lines) (map make-row-from-string) (apply vector)))
 
 (defn -main
   "Receive a filename through args and solve the content"
