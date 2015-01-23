4420e57353a5709dfe22d99ba52c01b27dac8803
ulysses-sl <mail@saklee.net>
2015-01-22 22:10:15 -0500

    Finished board loading/initialization/winning check logics

--- spec/sudoku_clj/core_spec.clj
+++ spec/sudoku_clj/core_spec.clj
@@ -2,13 +2,14 @@
   (:require [speclj.core :refer :all]
             [sudoku-clj.core :refer :all]))
 
-(describe "make-row-from-string"
+(describe "parse-row-from-string"
   (it "converts  \"1 2 3\" to [1 2 3]"
-    (should= '[1 2 3] (make-row-from-string "1 2 3"))))
+    (should= '[1 2 3] (parse-row-from-string "1 2 3"))))
 
-(describe "make-board-from-string"
-  (it "converts \"1 2 3  to [[1 2 3]\n                                      4 5 6\"     [4 5 6]]"
-    (should= '[[1 2 3] [4 5 6]] (make-board-from-string "1 2 3\n4 5 6"))))
+(describe "parse-board-from-string"
+  (it "converts \"1 2 3  to [[1 2 3]
+                                    4 5 6\"     [4 5 6]]"
+    (should= '[[1 2 3] [4 5 6]] (parse-board-from-string "1 2 3\n4 5 6"))))
 
 (describe "rows"
   (it "converts [[1 2 3] [4 5 6] [7 8 9]] to [[1 2 3] [4 5 6] [7 8 9]]"
@@ -18,9 +19,29 @@
   (it "converts [[1 2 3] [4 5 6] [7 8 9]] to [[1 4 7] [2 5 8] [3 6 9]]"
     (should= '[[1 4 7] [2 5 8] [3 6 9]] (columns '[[1 2 3] [4 5 6] [7 8 9]]))))
 
+(describe "group-by-3"
+  (it "groups [1 2 3 4 5 6] into [[1 2 3] [4 5 6]]"
+    (should= '[[1 2 3] [4 5 6]] (group-by-3 '[1 2 3 4 5 6]))))
+
+(describe "three-threes-from-3-rows"
+  (it "converts [[1 2 3 1 2 3]  to [[1 2 3 4 5 6 7 8 9]
+                                      [4 5 6 4 5 6]      [1 2 3 4 5 6 7 8 9]]
+                                      [7 8 9 7 8 9]]"
+    (should= '[[1 2 3 4 5 6 7 8 9] [1 2 3 4 5 6 7 8 9]]
+             (three-threes-from-3-rows '[[1 2 3 1 2 3] [4 5 6 4 5 6] [7 8 9 7 8 9]]))))
+
 (describe "three-threes"
-  (it "converts [[1 2 3 1 2 3] [4 5 6 4 5 6] [7 8 9 7 8 9]] to [[1 2 3 4 5 6 7 8 9] [1 2 3 4 5 6 7 8 9]]"
-    (should= '[[1 2 3 4 5 6 7 8 9] [1 2 3 4 5 6 7 8 9]] (three-threes '[[1 2 3 1 2 3] [4 5 6 4 5 6] [7 8 9 7 8 9]]))))
+  (it "converts [[1 2 3 1 2 3]  to [[1 2 3 4 5 6 7 8 9]
+                          [4 5 6 4 5 6]      [1 2 3 4 5 6 7 8 9]
+                          [7 8 9 7 8 9]      [1 2 3 4 5 6 7 8 9]
+                          [1 2 3 1 2 3]      [1 2 3 4 5 6 7 8 9]]
+                          [4 5 6 4 5 6]
+                          [7 8 9 7 8 9]]"
+    (should= '[[1 2 3 4 5 6 7 8 9] [1 2 3 4 5 6 7 8 9] [1 2 3 4 5 6 7 8 9] [1 2 3 4 5 6 7 8 9]]
+             (three-threes '[[1 2 3 1 2 3] [4 5 6 4 5 6] [7 8 9 7 8 9] [1 2 3 1 2 3] [4 5 6 4 5 6] [7 8 9 7 8 9]]))))
+
+
+
 
 (describe "has-9"
   (it "returns true on 9-element vector [1 2 3 4 5 6 7 8 9]"
@@ -29,9 +50,9 @@
   (it "returns false on 7-element vector [1 2 3 4 5 6 7]"
     (should-not (has-9 [1 2 3 4 5 6 7]))))
 
-(describe "is-valid-square"
+(describe "is-square-valid"
   (it "returns true on a valid one-digit non-negative integer"
-    (should (is-valid-square 1))))
+    (should (is-square-valid 1))))
 
 (describe "is-everything-unique"
   (it "returns true on vector with unique elements"
@@ -40,20 +61,97 @@
   (it "returns false on vectors with duplicate elements"
     (should-not (is-everything-unique '[1 2 3 1]))))
 
-(describe "is-valid-line"
+(describe "get-non-zeros"
+  (it "returns only the non-zero elements of a collection"
+    (should= '[1 2 3 4 5] (get-non-zeros '[1 0 2 3 0 0 4 5 0]))))
+
+
+
+(describe "all-true"
+  (it "returns true when everything in the collection is true"
+    (should (all-true '(true true true))))
+
+  (it "returns false when anything in the collection is not true"
+    (should-not (all-true '(true false true)))))
+
+(describe "is-line-valid"
   (it "returns true on a vector with 9 valid unique squares"
-    (should (is-valid-line '[1 2 3 4 5 6 7 8 9]))))
+    (should (is-line-valid '[1 2 3 4 5 0 7 8 9]))))
 
-(describe "are-rows-valid"
-  (it "returns true on [[1 2 3 4 5 6 7 8 9]]"
-    (should (are-rows-valid [[1 2 3 4 5 6 7 8 9]]))))
+(describe "check-all-lines"
+  (it "returns true when all the lines pass the check function"
+    (should (check-all-lines all-true [[true true] [true true]]))))
 
-(describe "are-columns-valid"
-  (it "returns true on [[1] [2] [3] [4] [5] [6] [7] [8] [9]]"
-    (should (are-columns-valid [[1] [2] [3] [4] [5] [6] [7] [8] [9]]))))
+(describe "check-board-position"
+  (it "returns true when all the rows pass the check function"
+    (should ((check-board-position is-line-valid rows) [[1 2 3 4 5 6 7 8 9]])))
 
-(describe "are-three-threes-valid"
-  (it "returns true on [[1 2 3] [4 5 6] [7 8 9]]"
-    (should (are-three-threes-valid [[1 2 3] [4 5 6] [7 8 9]]))))
+  (it "returns true when all the columns pass the check function"
+    (should ((check-board-position is-line-valid columns) [[1] [2] [3] [4] [5] [6] [7] [8] [9]])))
+
+  (it "returns true when all the 3x3s pass the check function"
+    (should ((check-board-position is-line-valid three-threes) [[1 2 3] [4 5 6] [7 8 9]]))))
+
+(describe "check-board"
+  (it "returns true when the entire board passes the check function"
+    (should (check-board all-true [[true true true] [true true true] [true true true]]))))
+
+
+
+(describe "make-guess-set-from-number"
+  (it "returns a set of integer 1 through 9 when 0 is received"
+    (should= #{1} (make-guess-set-from-number 1)))
+
+  (it "returns a set of integer 1 through 9 when 0 is received"
+    (should= #{1 2 3 4 5 6 7 8 9} (make-guess-set-from-number 0))))
+
+(describe "make-row-of-sets"
+  (it "receives a collection of numbers and returns row of sets"
+    (should= '[#{1} #{2} #{1 2 3 4 5 6 7 8 9} #{3}] (make-row-of-sets '[1 2 0 3]))))
+
+(describe "make-board-of-sets"
+  (it "receives a board of integers and returns a board of sets"
+    (should= '[[#{1} #{2}] [#{3} #{4}]] (make-board-of-sets [[1 2] [3 4]]))))
+
+
+
+(describe "is-line-solved"
+  (it "returns true when a collection of 9 unique singleton sets are given"
+    (should (is-line-solved [#{1} #{2} #{3} #{4} #{5} #{6} #{7} #{8} #{9}]))))
+
+
+(def row-3-5 '[[3 0] [3 1] [3 2] [3 3] [3 4] [3 5] [3 6] [3 7] [3 8]])
+(def column-3-5 '[[0 5] [1 5] [2 5] [3 5] [4 5] [5 5] [6 5] [7 5] [8 5]])
+(def three-three-3-5 '[[3 3] [3 4] [3 5] [4 3] [4 4] [4 5] [5 3] [5 4] [5 5]])
+(def combined-3-5 (->> (concat row-3-5 column-3-5 three-three-3-5) (distinct) (remove #(= '[3 5] %))))
+
+(describe "get-member-row"
+  (it "returns all coords on the same row"
+    (should= row-3-5 (get-member-row [3 5]))))
+
+(describe "get-member-column"
+  (it "returns all coords on the same column"
+    (should= column-3-5 (get-member-column [3 5]))))
+
+(describe "round-down-by"
+  (it "returns first number rounded down by the second number"
+    (should= 9 (round-down-by 11 3))))
+
+(describe "get-upper-left-corner-in-three-three"
+  (it "returns the coord of upper left corner square of current 3x3"
+    (should= '[3 3] (get-upper-left-corner-in-three-three [3 5]))))
+
+(describe "get-member-three-three"
+  (it "returns all coords on the same 3x3"
+    (should= three-three-3-5 (get-member-three-three [3 5]))))
+
+(describe "get-related-squares"
+  (it "returns all related coords on row, column, 3x3 without itself"
+    (should= combined-3-5 (get-related-squares [3 5]))))
+
+
+
+
+
 
 (run-specs)
--- src/sudoku_clj/core.clj
+++ src/sudoku_clj/core.clj
@@ -2,65 +2,240 @@
   (:gen-class)
   (:require [clojure.string :as str]))
 
+;;
+;; Board loading and sanity checking routines
+;;
+
 (def board-file "board.txt")
 
 (defn load-board-file
   []
   (slurp board-file))
 
-(defn make-row-from-string
+(defn parse-row-from-string
   "Reconstructs a single row of sudoku board from the string"
   [row-string]
-  (->> (str/split row-string #" ") (map read-string) (apply vector)))
+  (->> (str/split row-string #" ")
+       (map read-string)
+       (vec)))
 
-(defn make-board-from-string
+(defn parse-board-from-string
   "Reconstructs a sudoku board from the string"
   [board-string]
-  (->> board-string (str/split-lines) (map make-row-from-string) (apply vector)))
+  (->> board-string
+       (str/split-lines)
+       (map parse-row-from-string)
+       (vec)))
 
 (defn rows
+  "Returns the rows of the board"
   [board]
-  false)
+  board)
 
 (defn columns
+  "Returns the columns of the board"
   [board]
-  false)
+  (apply map vector board))
+
+(defn group-by-3
+  "Groups the given collection by 3"
+  [coll]
+  (partition 3 coll))
+
+(defn three-threes-from-3-rows
+  "Gets 3x3s from given 3 rows"
+  [rows]
+  (->> rows
+       (map group-by-3)
+       (columns)
+       (map #(apply concat %))))
 
 (defn three-threes
+  "Returns the 3x3s of the board"
   [board]
-  false)
+  (->> board
+       (group-by-3)
+       (mapcat three-threes-from-3-rows)))
+
+
 
 (defn has-9
+  "Checks if the collection has 9 items"
   [coll]
-  false)
+  (= 9 (count coll)))
 
-(defn is-valid-square
+(defn is-square-valid
+  "Checks if the given square x is an integer from 0 to 9"
   [x]
-  false)
+  (and (integer? x) (<= 0 x 9)))
 
 (defn is-everything-unique
-  [x]
-  false)
+  "Checks if every item in the collection is unique"
+  [coll]
+  (= coll (distinct coll)))
 
-(defn is-valid-line
+(defn get-non-zeros
+  "Gets non-zero items from the collection"
+  [coll]
+  (filter #(not= 0 %) coll))
+
+
+
+(defn all-true
+  "Checks if everything in the collection is true"
+  [coll]
+  (reduce #(and %1 %2) true coll))
+
+(defn is-line-valid
+  "Checks if a line has 9 numbers and all the non-zero nums are unique"
   [line]
-  false)
+  (and (->> line
+            (map is-square-valid)
+            (all-true))
+       (->> line
+            (get-non-zeros)
+            (is-everything-unique))))
 
-(defn are-rows-valid
-  [board]
-  false)
+(defn check-all-lines
+  "Checks every line to see if the condition meets"
+  [condition board]
+  (->> board
+       (map condition)
+       (all-true)))
 
-(defn are-columns-valid
-  [board]
-  false)
+(defn check-board-position
+  "Returns closure for checking the corresponding board positions for condition"
+  [condition position]
+  (fn [board]
+    (->> board
+         (position)
+         (check-all-lines condition))))
 
-(defn are-three-threes-valid
-  [board]
-  false)
+(defn check-board
+  "Checks the entire board for the condition"
+  [condition board]
+  (and ((check-board-position condition rows) board)
+       ((check-board-position condition columns) board)
+       ((check-board-position condition three-threes) board)))
 
 (defn is-board-valid
   [board]
+  (check-board is-line-valid board))
+
+;;
+;; Board preprocess
+;;
+
+(def set-of-possible-nums #{1 2 3 4 5 6 7 8 9})
+
+(defn make-guess-set-from-number
+  "Return guess sets if 0 or singleton set of itself otherwise"
+  [num]
+  (if (= 0 num)
+    set-of-possible-nums
+    #{num}))
+
+(defn make-row-of-sets
+  [row]
+  (->> row
+       (map make-guess-set-from-number)
+       (vec)))
+
+(defn make-board-of-sets
+  [board]
+  (->> board
+       (map make-row-of-sets)
+       (vec)))
+
+;;
+;; Check board consistency
+;;
+
+(defn is-line-solved
+  "Checks if line has permutation of (1..9)"
+  [line]
+  (= #{1 2 3 4 5 6 7 8 9}
+     (->> line (apply concat) (set))))
+
+(defn is-board-solved
+  "Checks if all combos in the board is solved"
+  [board]
+  (check-board is-line-solved board))
+
+;; All the board positions
+(def board-positions
+  (let [coords (take 9 (range))]
+    (for [row coords
+          column coords]
+      [row column])))
+
+;; Get the value at the specific coordinate of board
+(def get-square get-in)
+
+;; Set a square in the board to given value
+(def set-square assoc-in)
+
+(defn get-member-row
+  "Get all coords on the same row"
+  [[row x]]
+  (for [column (take 9 (range))]
+    [row column]))
+
+(defn get-member-column
+  "Get all coords on the same column"
+  [[x column]]
+  (for [row (take 9 (range))]
+    [row column]))
+
+(defn round-down-by
+  "Round down first number to second number's multiple"
+  [num div]
+  (- num (mod num div)))
+
+(defn get-upper-left-corner-in-three-three
+  "Get the coord of ULC in 3x3"
+  [[row column]]
+  (let [ulc-row (round-down-by row 3)
+        ulc-column (round-down-by column 3)]
+      [ulc-row ulc-column]))
+
+(defn get-member-three-three
+  "Get all coords on the same 3x3"
+  [coord]
+  (let [[ulc-row ulc-column]
+        (get-upper-left-corner-in-three-three coord)]
+    (for [new-row (range ulc-row (+ ulc-row 3))
+          new-column (range ulc-column (+ ulc-column 3))]
+      [new-row new-column])))
+
+(defn get-related-squares
+  "Get all related coords on row, column, 3x3 without itself"
+  [coord]
+  (let [adjacents
+        (-> (concat
+              (get-member-row coord)
+              (get-member-column coord)
+              (get-member-three-three coord))
+            (distinct))]
+    (remove #(= coord %) adjacents)))
+
+;;(some #(= 0 (count %)) coll)
+
+(defn mrv
+  "minimum remaining values"
+  []
   false)
+
+(defn lcv
+  "least constraining values"
+  []
+  false)
+
+(defn backtrack
+  []
+  '[])
+
+
 
 (defn -main
   "Receive a filename through args and solve the content"
